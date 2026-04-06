b63b3928582c4075f9080e93f744fddabc70c285
Luis Missbach <lmissbach@stud.hs-heilbronn.de>
2026-04-06 10:57:06 +0200

    Resolve "Adjust layout of App"

--- src/components/scenes/start-scene.js
+++ src/components/scenes/start-scene.js
@@ -8,21 +8,13 @@ class StartScene extends HTMLElement {
 
     // html body
     this.innerHTML = `
-          <div class="flex flex-col items-center justify-center min-h-screen font-sans bg-[#0d264f]">
+          <div class="flex flex-col items-center justify-center h-full w-full bg-blue-ud bg-cover">
             
-            <div class="w-64 h-64 md:w-80 md:h-80 rounded-full bg-white flex items-center justify-center shadow-2xl mb-12">
-              <img src="assets/textures/icons-and-logos/Logo_Kite.png" alt="Kite Emblem" class="object-contain w-full h-full">
-            </div>
+            <img src="assets/Images/LoadingScreen/Kite_Logo_im_Kreis.png" alt="Kite Emblem" class="pointer-events-none select-none object-contain w-160 h-160 mb-15">
 
-            <h1 class="text-8xl md:text-9xl font-black text-white lowercase tracking-tighter mb-16">
-              kite
-            </h1>
+            <img src="assets/Images/IconsAndLogos/Logo_Kite_Lettering_White.png" alt="Kite Text" class="pointer-events-none select-none object-contain w-170 h-85 mb-30">
 
-            <button id="start-btn" 
-                    class="px-12 py-4 border-2 border-white bg-[#123460] text-white text-xl font-bold tracking-[0.3em] uppercase hover:bg-white hover:text-[#0d264f] transition-all duration-300 shadow-lg cursor-pointer">
-              STARTEN
-            </button>
-
+            <button id="start-btn" class="w-81 h-23 border-5 border-white text-white text-3xl rounded-md mb-20"> STARTEN
           </div>
         `;
 
