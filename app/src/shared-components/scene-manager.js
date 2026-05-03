9c2d9c77ed3d7ff5742cbf9009a875036667381c
Veljko Belic <vbelic@stud.hs-heilbronn.de>
2026-05-03 18:28:59 +0200

    Resolve "Implement novel back button pop-up"

--- app/src/shared-components/scene-manager.js
+++ app/src/shared-components/scene-manager.js
@@ -1,6 +1,6 @@
 import "../scenes/start-scene.js";
 import "../scenes/settings-scene.js";
-import "../scenes/novel-scene.js";
+import "../scenes/novel-scene/novel-scene.js";
 import "../scenes/novel-selector.js";
 import "../scenes/novel-selector-sidebar.js";
 import "../scenes/legal_information/legal-information-scene.js";
